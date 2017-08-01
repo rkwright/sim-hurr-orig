0ec61680e17179bba1110f1b18dde0bd4c301e2d
Ric Wright <rkwright@geofx.com>
2017-08-01 10:57:09 -0500

    Added option to display the camera position info in the lower-right corner of the screen.  Added the flag to enable this into Basin3DTest.

--- gfx/gfx-scene.js
+++ gfx/gfx-scene.js
@@ -14,6 +14,7 @@ GFX.Scene = function ( parameters ) {
 	this.scene = null;
 	this.renderer = null;
     this.containerID = null;
+    this.container = undefined;
     this.shadowMapEnabled = false;
     this.alphaBuffer = false;
 
@@ -34,6 +35,7 @@ GFX.Scene = function ( parameters ) {
     this.far = 1000;
 	this.cameraPos = [0,20,40];
     this.orthoSize = 1;
+    this.cameraInfo = undefined;
 
 	this.controls = false;
 	this.orbitControls = [];
@@ -100,7 +102,6 @@ GFX.setParameters= function( object, values ) {
             else {
                 object[ key ] = newValue;
             }
-
         }
     }
 };
@@ -154,18 +155,17 @@ GFX.Scene.prototype = {
 		}
 	
 		// if the caller supplied the container elm ID try to find it
-		var container;
 		if (this.containerID !== null && typeof this.containerID !== 'undefined')
-			container = document.getElementById(this.containerID);
+			this.container = document.getElementById(this.containerID);
 		
 		// couldn't find it, so create it ourselves
-		if (container === null || typeof container === 'undefined') {
-			container = document.createElement( 'div' );
-			document.body.appendChild( container );
+		if (this.container === null || typeof this.container === 'undefined') {
+			this.container = document.createElement( 'div' );
+			document.body.appendChild( this.container );
 		}
 		else {
-			this.canvasWidth = container.clientWidth;
-			this.canvasHeight = container.clientHeight;
+			this.canvasWidth = this.container.clientWidth;
+			this.canvasHeight = this.container.clientHeight;
 		}
 	
 		// allocate the THREE.js renderer
@@ -186,17 +186,12 @@ GFX.Scene.prototype = {
 		this.renderer.setSize(this.canvasWidth, this.canvasHeight);
 	
 		// Get the DIV element from the HTML document by its ID and append the renderer's DOM object
-		container.appendChild(this.renderer.domElement);
+		this.container.appendChild(this.renderer.domElement);
 
 		// if the user hasn't set defaultLights to false, then set them up
 		if (this.defaultLights === true)
 		    this.setDefaultLights();
 
-		// request the orbitControls be created and enabled
-		// add the controls
-		//if (this.controls === true && this.renderer !== null)
-		//    this.setDefaultControls();
-		
 		if ( this.axesHeight !== 0 )
 			this.drawAxes(this.axesHeight);
 		
@@ -204,7 +199,7 @@ GFX.Scene.prototype = {
 			this.addFloor(this.floorRepeat);
 
         // set up the stats window(s) if requested
-		this.setupStats( container );
+		this.setupStats( this.container );
 	},
 
 	add: function ( obj ) {
@@ -272,7 +267,7 @@ GFX.Scene.prototype = {
             if (jsonObj.perspective !== undefined)
                 perspective = jsonObj.perspective;
 
-            cameraPos   = jsonObj.cameraPos || this.camerPos;
+            cameraPos   = jsonObj.cameraPos || this.cameraPos;
             fov         = jsonObj.fov || this.fov;
             near        = jsonObj.near || this.near;
             far         = jsonObj.far || this.far;
@@ -302,15 +297,22 @@ GFX.Scene.prototype = {
 
         this.scene.add(camera);
 
-        if (this.controls === true && this.renderer !== null) {
-            this.orbitControls[this.cameras.length-1] = new THREE.OrbitControls(camera, this.renderer.domElement);
-        }
-
         // set the "default" camera if not already done
         if (this.camera === undefined)
             this.camera = camera;
 
+        this.addControls( camera );
+        //this.updateCameraInfo();
+
         return camera;
+    },
+
+    // set up the camera info string
+    updateCameraInfo: function () {
+
+        this.cameraInfoStr = this.camera.isPerspectiveCamera ? "P" : "O";
+        this.cameraInfoStr += " - x: " + this.camera.position.x.toFixed(1) + " y: " + this.camera.position.y.toFixed(1) +
+                    " z: "+ this.camera.position.z.toFixed(1) + "&nbsp;&nbsp;";
     },
 
     getCamera: function ( index ){
@@ -334,6 +336,13 @@ GFX.Scene.prototype = {
         if (camera !== null && typeof camera !== 'undefined') {
             this.orbitControls.push( new THREE.OrbitControls(camera, this.renderer.domElement) );
         }
+
+        if (this.cameraInfo !== undefined) {
+            this.cameraInfo = document.createElement( 'div' );
+            this.cameraInfo.style.cssText = 'position:fixed;bottom:0;right:0;opacity:0.9;z-index:10000;color:cyan;\
+                      font-size:12px;font-family:Helvetica,Arial,sans-serif';
+            this.container.appendChild( this.cameraInfo );
+        }
     },
 
     updateControls: function () {
@@ -341,6 +350,8 @@ GFX.Scene.prototype = {
         for ( var i=0; i<this.orbitControls.length; i++ ) {
             this.orbitControls[i].update();
         }
+
+        this.updateCameraInfo();
     },
 
     /**
@@ -539,6 +550,10 @@ GFX.Scene.prototype = {
             this.msStats.update();
         if (this.mbStats !== null && typeof this.mbStats !== 'undefined')
             this.mbStats.update();
+
+        if (this.cameraInfo !== undefined) {
+            this.cameraInfo.innerHTML = this.cameraInfoStr;
+        }
     },
 
 	addFog: function( values ) {
