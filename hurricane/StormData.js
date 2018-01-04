c5c6dfae9e66c178a1ce832d051d62e2a3e215d3
Ric Wright <rkwright@geofx.com>
2018-01-04 09:51:51 -0600

    Renamed StormData as StormJSON as it is about loading the JSON file.  Created new StormData file which mirrors the JSON format in the storm file.  Updated the script tags to in HurrDemo to reflect this.

--- hurricane/StormData.js
+++ hurricane/StormData.js
@@ -1,70 +1,25 @@
 /*
- * Storm-data loading facilities
+ * Storm Data for a single storm for the hurricane modelling
+ *
+ * This is the data for a single storm, comprising the metadata and all the
+ * entries.
  *
  * @author rkwright / www.geofx.com
  *
- * Copyright 2017, All rights reserved.
+ * Copyright 2018, All rights reserved.
+ *
  */
 
-/**
- * Constants
- */
-var StormData = {
-    revision: 'r01'
+var StormData= {
+    revision: "r01"
 };
 
 /**
  * @constructor
  */
 StormData.StormData = function () {
-    this.stormFile = undefined;
-    this.jsonData  = undefined;
 
-    stThis = this;
-};
-
-StormData.StormData.prototype = {
-
-    loadJSON: function(callback) {
-
-        var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
-        xobj.open('GET', this.stormFile, true);
-        xobj.onreadystatechange = function () {
-            if (xobj.readyState === XMLHttpRequest.HEADERS_RECEIVED) {
-                console.log(xobj.getAllResponseHeaders());
-            }
-
-            if (xobj.readyState === XMLHttpRequest.DONE && xobj.status === HttpStatus.OK) {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-                callback(xobj.responseText);
-            }
-        };
-        xobj.send(null);
-    },
-
-    /**
-     * Load the data from the specified JSON file, then parse the
-     * resuling payload
-     * @param stormFile
-     * @param stormReady
-     */
-    loadData: function( stormFile, stormReady ) {
-
-        this.stormFile = stormFile;
-        this.stormReady = stormReady;
-        this.loadJSON(function(response) {
-            // Parse JSON string into object
-            this.jsonData = JSON.parse(response);
-            stThis.stormReady();
-        });
-    },
-
-    /**
-     * Getter for the JSON data
-     * @returns {*}
-     */
-    getJSON:  function () {
-        return this.jsonData;
-    }
+    this.atcID   = undefined;
+    this.name    = undefined;
+    this.entries = [];
 };
