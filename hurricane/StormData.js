13897982e01d06400b9bc6cf363978177dec5591
Ric Wright <rkwright@geofx.com>
2018-02-01 10:27:06 -0600

    Added Saffir-Simpson table and method to StormData.  Added a Saffir-Simpson driven set of materials for coloring the storm tracks.  Working fairly well at this point.

--- hurricane/StormData.js
+++ hurricane/StormData.js
@@ -10,6 +10,16 @@
 var STORMDATA = {
     revision: "1.0",
 
+    SAFFIR : [
+        { cat: '5',  minMPH: 157, color: 0xff6060 },
+        { cat: '4',  minMPH: 130, color: 0xff8f20 },
+        { cat: '3',  minMPH: 111, color: 0xffc140 },
+        { cat: '2',  minMPH: 96,  color: 0xffe775 },
+        { cat: '1',  minMPH: 74,  color: 0xffffcc },
+        { cat: 'TS', minMPH: 39,  color: 0x01faf4 },
+        { cat: 'TD', minMPH: 33,  color: 0x5dbaff }
+    ],
+
     // contents of the entry fields
     YEAR:     0,
     MONTH:    1,
@@ -33,4 +43,21 @@ StormData = function () {
     this.atcID   = undefined;
     this.name    = undefined;
     this.entries = [];
+};
+
+StormData.prototype = {
+
+    /**
+     * Return the Saffir-Simpson category for the specified windspeed, in MPH
+     * @param windSpeed
+     * @returns {*}
+     */
+    getSaffirCat: function (windSpeed) {
+        for (var i = 0; i < STORMDATA.SAFFIR.length - 1; i++) {
+            if (windSpeed >= STORMDATA.SAFFIR[i].minMPH)
+                break;
+        }
+
+        return i;
+    }
 };
