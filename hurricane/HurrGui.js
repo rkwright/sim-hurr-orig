8ae3c06dd82b2897fa58a9d397fe7642450ad00c
Ric Wright <rkwright@geofx.com>
2018-02-12 17:28:48 -0600

    Moved the dat.gui hurricane selection code off to its own class HurrGui, which uses Closures.

--- hurricane/HurrGui.js
+++ hurricane/HurrGui.js
@@ -7,108 +7,143 @@
  *
  */
 
-var HURRGUI = {
-    revision: '1.0'
-};
+'use strict';
+var HurrGui = (function () {
 
-HurrGui = function ( gui, stormFile, stormCallback, udateCallback ) {
-    this.gui = gui;
-    this.stormCallback = stormCallback;
+    // Constructor
+    function HurrGui( gui, stormFile, updateCallback ) {
+        this.gui = gui;
+        this.stormFile = stormFile;
+        this.updateCallback = updateCallback;
 
-    this.options  = {};
-    this.curStorm = undefined;
-    this.storms = undefined;
+        this.options = {};
+        this.options.this = this;
+        this.curStorm = undefined;
+        this.storms = undefined;
 
-    this.stormsGui         = undefined;
-    this.updateStorms.gui  = undefined;
-    this.updateEntries.gui = undefined;
+        this.stormsGui = undefined;
 
-    this.parmsGui  = undefined;
-};
+        this.parmsGui = undefined;
 
-HurrGui.prototype = {
-    /**
-     * Update the existing controller for storms and create a new one.
-     * Have to do it this way as there appears to be no easy way to
-     * "refresh" the data in a controller
-     */
-    updateStorms: function ( year) {
-
-        if (this.updateStorms.gui !== undefined)
-            this.gui.remove(this.updateStorms.gui);
-
-        this.storms = stormFile.getStormsForYear(Number(year));
-        this.stormLabels = stormFile.getStormLabels(storms);
-        this.options.stormLabels = stormLabels[0];
-
-        this.updateStorms.gui = gfxScene.gui.add(options, "stormLabels", this.stormLabels).name("Storms");
-        this.updateStorms.gui.onChange(stormsChange);
-    },
-
-    /**
-     * Handle the change event for the storms controller
-     */
-    stormsChange: function () {
-        var index = this.stormLabels.indexOf( this.options.stormLabels );
-        this.curStorm = storms[index];
-        this.updateEntries( this.curStorm );
-        this.stormCallback( this.curStorm );
-    },
-
-    /**
-     * Update existing controller for the entries and create a new one
-     */
-    updateEntries: function ( storm ) {
-        if (this.updateEntries.gui !== undefined)
-            this.gui.remove( this.updateEntries.gui );
-
-        this.entryLabels = stormFile.getEntryLabels(storm);
-        this.options.entryLabels = entryLabels[0];
-
-        this.updateEntries.gui = gfxScene.gui.add(options, "entryLabels", entryLabels).name("Entries");
-        this.updateEntries.gui.onChange(entriesChange);
-    },
-
-    /**
-     * Handle the change event for the entries controller.  Not used yet.
-     */
-    entriesChange: function () {
-        var index = this.entryLabels.indexOf( this.options.entryLabels );
-    },
-
-    /**
-     * Set up the datgui controls on the basis of the loaded storm data
-     */
-    setupGuiStorms: function () {
-        this.stormGui = this.gui.addFolder("Storms");
-
-        this.years = this.stormFile.getYears();
-        this.storms = this.stormFile.getStormsForYear( this.years[0] );
-        this.stormLabels = this.stormFile.getStormLabels( this.storms );
-        this.entryLabels = this.stormFile.getEntryLabels( this.storms[0] );
-
-        this.options.year = years[0];
-        this.options.stormLabels = this.stormLabels[0];
-        this.options.entryLabels = this.entryLabels[0];
-        this.options.update = function () {
-            this.updateCallback();
-        };
-
-        var gui_year = stormGui.add(options, "year", years).name("Year").onChange(function () {
-            console.log("Changed year");
-            this.updateStorms( options.year );
-            this.updateEntries( storms[0] );
-        });
-
-        this.updateStorms( this.options.year );
-
-        this.curStorm = this.storms[0];
-        updateEntries( this.curStorm );
-
-        this.stormGui.add( this.options, 'update' );
-    },
-
-    setupDatGui: function () {
-
+        window.gThis = this;
     }
-};
+
+    HurrGui.prototype = {
+
+        // Constants
+        REVISION: '1.0',
+
+        /**
+         * Update the existing controller for storms and create a new one.
+         * Have to do it this way as there appears to be no easy way to
+         * "refresh" the data in a controller
+         */
+        updateStorms: function (year) {
+
+            if (this.updateStorms.gui !== undefined)
+                this.stormGui.remove(this.updateStorms.gui);
+
+            this.storms = this.stormFile.getStormsForYear(Number(year));
+            this.curStorm = this.storms[0];
+            this.stormLabels = this.stormFile.getStormLabels(this.storms);
+            this.options.stormLabels = this.stormLabels[0];
+
+            this.updateStorms.gui = this.stormGui.add(this.options, "stormLabels", this.stormLabels).name("Storms");
+            this.updateStorms.gui.onChange(this.stormsChange);
+        },
+
+        /**
+         * Handle the change event for the storms controller
+         */
+        stormsChange: function () {
+            var gThis = window.gThis;
+            var index = gThis.stormLabels.indexOf( gThis.options.stormLabels );
+            gThis.curStorm = gThis.storms[index];
+            gThis.updateEntries( gThis.curStorm );
+            gThis.updateButton();
+            //gThis.updateCallback( gThis.curStorm );
+        },
+
+        /**
+         * Update existing controller for the entries and create a new one
+         */
+        updateEntries: function (storm) {
+            if (this.updateEntries.gui !== undefined)
+                this.stormGui.remove(this.updateEntries.gui);
+
+            this.entryLabels = stormFile.getEntryLabels(storm);
+            this.options.entryLabels = this.entryLabels[0];
+
+            this.updateEntries.gui = this.stormGui.add(this.options, "entryLabels", this.entryLabels).name("Entries");
+            this.updateEntries.gui.onChange(this.entriesChange);
+        },
+
+        /**
+         * Handle the change event for the entries controller.  Not used yet.
+         */
+        entriesChange: function () {
+            var gThis = window.gThis;
+            var index = gThis.entryLabels.indexOf( gThis.options.entryLabels );
+        },
+
+        /**
+         * Handle change in the year combo-box
+         */
+        yearChange: function () {
+            console.log("Changed year");
+
+            var gThis = window.gThis;
+            gThis.updateStorms(gThis.options.year);
+            gThis.updateEntries(gThis.storms[0]);
+            gThis.updateButton();
+        },
+
+        /**
+         * Remove and renew the update "button"
+         */
+        updateButton: function() {
+           if (this.updateButton.gui !== undefined)
+               this.stormGui.remove(this.updateButton.gui);
+
+           this.updateButton.gui = this.stormGui.add(this.options, 'update');
+          this.options.update = function () {
+              window.gThis.updateCallback( window.gThis.curStorm );
+          };
+        },
+
+        /**
+         * Set up the datgui controls on the basis of the loaded storm data
+         */
+        setupGuiStorms: function () {
+            this.stormGui = this.gui.addFolder("Storms");
+
+            this.years = this.stormFile.getYears();
+            this.storms = this.stormFile.getStormsForYear(this.years[0]);
+            this.curStorm = this.storms[0];
+            this.stormLabels = this.stormFile.getStormLabels(this.storms);
+            this.entryLabels = this.stormFile.getEntryLabels(this.storms[0]);
+
+            this.options.year = this.years[0];
+            this.options.stormLabels = this.stormLabels[0];
+            this.options.entryLabels = this.entryLabels[0];
+            this.options.update = function () {
+                window.gThis.updateCallback( window.gThis.curStorm );
+            };
+
+            var gui_year = this.stormGui.add(this.options, "year", this.years).name("Year").onChange(this.yearChange);
+
+            this.updateStorms(this.options.year);
+
+            this.updateEntries(this.curStorm);
+
+            this.updateButton();
+        },
+
+        setupDatGui: function () {
+
+            this.setupGuiStorms();
+        }
+    };
+
+    return HurrGui;
+})();
---
+++ hurricane/HurrPlot.js
@@ -0,0 +1,26 @@
+/*
+ * Hurricane Plot setup.
+ *
+ * @author rkwright / www.geofx.com
+ *
+ * Copyright 2018, All rights reserved.
+ *
+ */
+
+'use strict';
+var HurrPlot = (function () {
+
+    // Constructor
+    function HurrPlot( curStorm, stormParms) {
+
+    }
+
+    HurrPlot.prototype = {
+
+        // Constants
+        REVISION: '1.0',
+
+    };
+
+    return HurrPlot;
+})();
